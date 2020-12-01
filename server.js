40e40753f02ea655a2077086540cc54270c6d67c
halaalsawaeer <h.sawaeer@yahoo.com>
2020-12-01 06:18:10 +0200

    lab12 didn't finish yet

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+
 'use strict';
 require('dotenv').config();
 const express = require('express');
 
 const PORT = process.env.PORT || 3030;
 const app = express();
+const pg = require('pg');
 const superagent = require('superagent');
-
+const client = new pg.Client(process.env.DATABASE_URL)
 app.use(express.static('./public'));
 
 
@@ -14,16 +16,49 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('pages/index');
+    res.redirect('/index')
 })
+
+app.get('/index', (req, res) => {
+    let SQL = 'SELECT * FROM books;';
+    return client.query(SQL)
+        .then(results => {
+            res.render('pages/index', { books: results.rows });
+        })
+
+})
+
+
 app.get('/search', (req, res) => {
-    res.render('pages/searches/new.ejs')
+    res.render('./pages/searches/new')
 })
+
+app.get('books/:id', (req, res) => {
+    let SQL = `SELECT *FROM books WHERE id =$1;`
+    let values = [req.params.id];
+    return client.query(SQL, values)
+        .then((results) => {
+            res.render('./pages/books/show', { books: results.rows[0] })
+        })
+})
+
+app.post('/books', (req, res)=> {
+    let SQL = `INSERT INTO books (title, author, isbn, image_url, description) VALUES ($1,$2,$3,$4,$5) RETURNING id;`
+    let { title, author, isbn, image_url, description } = req.body;
+    let safeValues = [title, author, isbn, image_url, description];
+    
+    client.query(SQL, safeValues)
+    .then((results) => {
+        res.redirect(`./pages/books/${results.rows[0].id}`)
+    })
+    
+});
+
 
 app.post('/searches', (req, res) => {
     var url;
     let bookSearch = req.body.bookname;
-    
+
     if (req.body.myBook === 'title') {
         url = `https://www.googleapis.com/books/v1/volumes?q=${bookSearch}&intitle:${bookSearch}`;
     }
@@ -35,27 +70,33 @@ app.post('/searches', (req, res) => {
         .then(data => {
             let arr = data.body.items;
             let books = arr.map(book => {
-                let bookObj = new Book(book);
+                return new Book(book);
                 
-                return bookObj;
             });
-            res.render('pages/searches/show', { books: books })
+            res.render('pages/searches/show', { bookData: books })
         })
-        .catch(error => {
-            res.render(error,'pages/error');
-        });
+        
 });
+
+
+
 function Book(book) {
     this.title = book.volumeInfo.title;
-    this.image = book.volumeInfo.imageLinks.smallThumbnail || `https://i.imgur.com/J5LVHEL.jpg`;
-    this.authors = book.volumeInfo.authors || 'Not avilabile';
-    this.description = book.volumeInfo.description || 'Not avilabile';
-
+    this.image = book.volumeInfo.imageLinks.smallThumbnail ? book.volumeInfo.imageLinks.smallThumbnail : `https://i.imgur.com/J5LVHEL.jpg`;
+    this.authors = book.volumeInfo.authors ? book.volumeInfo.authors[0] : 'Not avilabile';
+    this.description = book.volumeInfo.description ? book.volumeInfo.description : 'Not avilabile';
+    this.isbn = book.volumeInfo.industryIdentifiers[0].type;
+ 
 }
 app.get('*', (req, res) => {
-    res.status(404).send('You have a error in writing the route');
+    res.status(404).send('error error ! ');
 })
 
-app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`)
-})
+
+client.connect()
+    .then(() => {
+
+        app.listen(PORT, () => {
+            console.log(`Listening on PORT ${PORT}`)
+        });
+    });
