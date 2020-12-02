3973a7604b52262cebe98f97e728da6bdff337e4
halaalsawaeer <h.sawaeer@yahoo.com>
2020-12-02 08:57:06 +0200

    done

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 'use strict';
 require('dotenv').config();
 const express = require('express');
@@ -6,22 +5,20 @@ const express = require('express');
 const PORT = process.env.PORT || 3030;
 const app = express();
 const pg = require('pg');
+const methodOverride = require('method-override');
 const superagent = require('superagent');
 const client = new pg.Client(process.env.DATABASE_URL)
 app.use(express.static('./public'));
-
+app.use(methodOverride('_method'));
 
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.redirect('/index')
-})
 
-app.get('/index', (req, res) => {
+app.get('/', (req, res) => {
     let SQL = 'SELECT * FROM books;';
-    return client.query(SQL)
+  client.query(SQL)
         .then(results => {
             res.render('pages/index', { books: results.rows });
         })
@@ -33,12 +30,12 @@ app.get('/search', (req, res) => {
     res.render('./pages/searches/new')
 })
 
-app.get('books/:id', (req, res) => {
-    let SQL = `SELECT *FROM books WHERE id =$1;`
+app.get('/books/:id', (req, res) => {
+    let SQL = `SELECT * FROM books WHERE id =$1;`
     let values = [req.params.id];
-    return client.query(SQL, values)
+    client.query(SQL, values)
         .then((results) => {
-            res.render('./pages/books/show', { books: results.rows[0] })
+            res.render('pages/books/show', { books: results.rows[0] })
         })
 })
 
@@ -49,13 +46,36 @@ app.post('/books', (req, res)=> {
     
     client.query(SQL, safeValues)
     .then((results) => {
-        res.redirect(`./pages/books/${results.rows[0].id}`)
+        res.redirect(`/books/${results.rows[0].id}`)
     })
     
 });
+ 
+app.put('/updateBooks/:id', (req,res) =>{
+
+    let { title, author, isbn, image_url, description } = req.body;   
+    let SQL = `UPDATE books SET title=$1,author=$2,isbn=$3,image_url=$4,description=$5 WHERE id=$6;`
+    let safeValues = [title, author, isbn, image_url, description,req.params.id];
+    
+    client.query(SQL,safeValues)
+    .then(()=>{
+        res.redirect(`/books/${req.params.id}`)
+    })
+})
+
+app.delete('/deleteBook/:id', (req,res) =>{
+    let SQL = `DELETE FROM books WHERE id=$1;`
+    let values =[req.params.id]
+
+    client.query(SQL, values)
+    .then(()=>{
+        res.redirect(('/'))
+    })
+})
 
 
 app.post('/searches', (req, res) => {
+    
     var url;
     let bookSearch = req.body.bookname;
 
@@ -74,6 +94,10 @@ app.post('/searches', (req, res) => {
                 
             });
             res.render('pages/searches/show', { bookData: books })
+
+        })
+        .catch(error => {
+            res.render('pages/error');
         })
         
 });
@@ -82,7 +106,7 @@ app.post('/searches', (req, res) => {
 
 function Book(book) {
     this.title = book.volumeInfo.title;
-    this.image = book.volumeInfo.imageLinks.smallThumbnail ? book.volumeInfo.imageLinks.smallThumbnail : `https://i.imgur.com/J5LVHEL.jpg`;
+    this.image = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.smallThumbnail : `https://i.imgur.com/J5LVHEL.jpg`;
     this.authors = book.volumeInfo.authors ? book.volumeInfo.authors[0] : 'Not avilabile';
     this.description = book.volumeInfo.description ? book.volumeInfo.description : 'Not avilabile';
     this.isbn = book.volumeInfo.industryIdentifiers[0].type;
